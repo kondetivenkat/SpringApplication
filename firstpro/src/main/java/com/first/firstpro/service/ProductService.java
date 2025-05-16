711854f0797eca4547f4159d2a5a539e930f304c
venkat <131855816+kondetivenkat@users.noreply.github.com>
2025-05-16 23:17:38 +0530

    search done

--- firstpro/src/main/java/com/first/firstpro/service/ProductService.java
+++ firstpro/src/main/java/com/first/firstpro/service/ProductService.java
@@ -41,4 +41,9 @@ public class ProductService {
 	public void deleteProduct(int id) {
 		 repo.deleteById(id);
 	}
+
+	public List<Product> searchProducts(String keyword) {
+		// TODO Auto-generated method stub
+		return repo.searchProducts(keyword);
+	}
 }
